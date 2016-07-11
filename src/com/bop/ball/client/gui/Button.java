28eb8dba91d6ae48d5109ea6ae6774b7b8958bd6
YuHwut <tmoeb2@gmail.com>
2016-07-10 23:54:51 -0400

    Add files via upload

--- src/com/bop/ball/client/gui/Button.java
+++ src/com/bop/ball/client/gui/Button.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bop.ball.client.gui;
 
-/**
- *
- * @author student
- */
-public class Button {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Button extends GUIActionComponent
+{
+    @Override
+    public void onClick(float mouseX, float mouseY) {
+        
+    }
+
+    @Override
+    public void onKeyPress(int key) 
+    {
+        if(key == Input.KEY_ENTER) onClick(0,0);
+    }
+
+    @Override
+    public void render(GameContainer con, StateBasedGame game, Graphics g) throws SlickException {
+        
+    }
+
+    @Override
+    public void update(GameContainer con, StateBasedGame game, int delta) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
